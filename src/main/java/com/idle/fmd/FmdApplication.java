b7eae390163f47effa278b4fdb1a82e43db43a53
jyj9111 <mrjang9111@gmail.com>
2023-08-09 11:33:06 +0900

    Chore: first-init

---
+++ src/main/java/com/idle/fmd/FmdApplication.java
@@ -0,0 +1,13 @@
+package com.idle.fmd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FmdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FmdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/idle/fmd/FmdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.idle.fmd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FmdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
