88e15bb7a4949dd9beb598eb868a301af0f89f0e
jyj9111 <mrjang9111@gmail.com>
2023-08-13 16:20:48 +0900

    Refactor: 생성자 추가 - UserLoginResponseDto(String token)

--- src/main/java/com/idle/fmd/domain/user/dto/UserLoginResponseDto.java
+++ src/main/java/com/idle/fmd/domain/user/dto/UserLoginResponseDto.java
@@ -6,4 +6,8 @@ import lombok.Data;
 @Data
 public class UserLoginResponseDto {
     private String token;
+    
+    public UserLoginResponseDto(String token) {
+        this.token = token;
+    }
 }
