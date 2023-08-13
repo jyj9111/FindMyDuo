779d86a317262bf74df9c7fb6c9a19f43fa293a5
jyj9111 <mrjang9111@gmail.com>
2023-08-13 17:44:38 +0900

    Feat: OAuth2를 사용한 소셜로그인 기능 추가 - Naver, Kakao, Google 플랫폼 로그인 기능 추가 - WebSecurityConfig: oauth2 설정 추가 - UserController: oauth2 로그인 성공 유저 jwt 토큰발급용 엔드포인트 추가 - login-form.html: 소셜로그인 테스트를 위한 임시 로그인폼 추가 - ViewController: 임시 로그인폼 맵핑을 위한 Controller 추가 - OAuth2UserServiceImpl: OAuth2로 넘겨받은 사용자 정보 처리를 위한 Service 클래스 추가 - OAuth2SuccessHandler: OAuth2 인증성공 후의 동작을 담당하는 Handler 클래스 추가

--- src/main/java/com/idle/fmd/domain/user/controller/UserController.java
+++ src/main/java/com/idle/fmd/domain/user/controller/UserController.java
@@ -2,21 +2,25 @@ package com.idle.fmd.domain.user.controller;
 
 import com.idle.fmd.domain.user.dto.UserLoginRequestDto;
 import com.idle.fmd.domain.user.service.UserService;
-import com.idle.fmd.global.auth.JwtTokenDto;
+import com.idle.fmd.domain.user.dto.UserLoginResponseDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/users")
 public class UserController {
     private final UserService userService;
-    @PostMapping("/login")
-    public JwtTokenDto login(@RequestBody UserLoginRequestDto dto) {
 
+    // 로그인 유저 토큰 발급
+    @PostMapping("/login")
+    public UserLoginResponseDto login(@RequestBody UserLoginRequestDto dto) {
         return userService.loginUser(dto);
+    }
+
+    // OAuth 로그인 유저 토큰 발급
+    @GetMapping("/oauth")
+    public UserLoginResponseDto oauthLogin(@RequestParam("token") String token) {
+        return new UserLoginResponseDto(token);
     }
 }
