5983da0e20a0f807005e8e717750d7596a5efe02
하늘 <gureumi74@naver.com>
2023-08-27 21:45:53 +0900

    Refactor: LolAccountEntity 와 LolMatchEntity 연관관계 맺기

--- src/main/java/com/idle/fmd/domain/lol/entity/LolAccountEntity.java
+++ src/main/java/com/idle/fmd/domain/lol/entity/LolAccountEntity.java
@@ -4,6 +4,8 @@ import com.idle.fmd.domain.user.entity.UserEntity;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.util.List;
+
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -52,6 +54,11 @@ public class LolAccountEntity {
     @OneToOne(fetch = FetchType.LAZY, mappedBy = "lolAccount", cascade = CascadeType.REMOVE, orphanRemoval = true)
     @Setter
     private LolInfoEntity lolInfo;
+
+    // 롤 계정 정보와 롤 전적 정보 연관관계, 부모가 지워지면 자식도 지워지도록 설정
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "lolAccount", orphanRemoval = true)
+    @Setter
+    private List<LolMatchEntity> lolMatch;
 
     // Lol 과 User 의 연관관게 편의 메소드
     public void addLolAccountUser(UserEntity user) {
