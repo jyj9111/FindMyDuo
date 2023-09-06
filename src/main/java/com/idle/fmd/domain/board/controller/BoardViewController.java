cadadf5757359ae8586153e9dbfe139704e3f68b
GrO0vy <lmc2819@naver.com>
2023-09-06 16:41:59 +0900

    Feat: 자유게시판 관련 HTML 을 띄우는 컨트롤러 추가 - 글 작성 / 수정 폼으로 이동하는 URL 추가

---
+++ src/main/java/com/idle/fmd/domain/board/controller/BoardViewController.java
@@ -0,0 +1,24 @@
+package com.idle.fmd.domain.board.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@RequestMapping("/board")
+@RequiredArgsConstructor
+@Controller
+public class BoardViewController {
+    // 글 작성 폼으로 이동
+    @GetMapping("/form/write")
+    public String writeForm() {
+        return "/board/board-write-form";
+    }
+
+    // 글 수정 폼으로 이동
+    @GetMapping("/form/{boardId}")
+    public String updateForm() {
+        return "/board/board-update-form";
+    }
+}
