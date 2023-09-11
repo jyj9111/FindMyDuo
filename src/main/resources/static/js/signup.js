6fb7f8e393100a4fc3265f1ba5d871e7760bd737
하늘 <gureumi74@naver.com>
2023-09-11 10:20:57 +0900

    Design: 회원가입 폼 UI 개선 (help 메시지 출력) - WebSecurityConfig: 중복 확인을 위한 메서드를 담은 users/check/** 접근 허용 - signup.html: 각종 help 메시지 출력, 모든 입력 폼을 입력해야지만 버튼이 눌리도록 변경 - signup.js: help 메시지를 출력하기 위한 js 코드 추가 - UserController: 아이디 중복확인, 닉네임 중복확인, 이메일 중복확인 컨트롤러 추가 - UserService: 컨트롤러에 맞는 서비스 로직 추가

--- src/main/resources/static/js/signup.js
+++ src/main/resources/static/js/signup.js
@@ -8,11 +8,70 @@ document.addEventListener("DOMContentLoaded", function () {
             nickname: '',
             password: '',
             passwordCheck: '',
+            accountIdErrorMessage: '', // 아이디 중복 확인 표시 데이터
+            emailErrorMessage: '', // 이메일 오류 메시지를 표시할 데이터
+            emailAuthCodeErrorMessage: '', // 이메일 확인 오류 메시지를 표시할 데이터
+            nicknameErrorMessage: '', // 닉네임 오류 메시지를 표시할 데이터
+            passwordErrorMessage: '', // 패스워드 오류 메시지를 표시할 데이터
+            passwordCheckErrorMessage: '', // 패스워드 체크 오류 메시지를 표시할 데이터
+            isFormValid: false, // 회원가입 버튼
+        },
+        watch: {
+            passwordCheck: 'checkFormValidity',
+            passwordCheckErrorMessage: 'checkFormValidity'
         },
         methods: {
+            // 아이디 폼 확인 메서드
+            async checkExistingAccount() {
+                const accountId = this.accountId;
+                const response = await axios.get('/users/check/accountId', {
+                    params: {accountId},
+                })
+
+                console.log(response.data);
+
+                if(response.data) {
+                    this.accountIdErrorMessage = '이미 가입한 회원의 아이디입니다.'
+                } else {
+                    this.accountIdErrorMessage = '';
+                }
+            },
+            // 이메일 폼 확인 메서드
+            async checkEmailFormat() {
+                // 이메일 형식 검사
+                const emailRegex = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/;
+                if(!emailRegex.test(this.email)) {
+                    this.emailErrorMessage = '올바른 이메일 형식을 입력하세요.';
+                } else {
+                    this.emailErrorMessage = '';
+                }
+            },
+            // 이메일 중복 확인 메서드
+            async checkExistingEmail() {
+                const email = this.email;
+                const response = await axios.get('/users/check/email', {
+                    params: {email},
+                })
+
+                console.log(response.data);
+
+                if(response.data) {
+                    this.emailErrorMessage = '이미 등록된 회원의 이메일입니다.';
+                }
+            },
+            // 이메일 확인 폼 확인 메서드
+            async checkEmailAuthCode() {
+                const code = this.emailAuthCode;
+
+                if(code.length != 6) {
+                    this.emailAuthCodeErrorMessage = '인증번호 코드는 6글자입니다.';
+                } else {
+                    this.emailAuthCodeErrorMessage = '';
+                }
+            },
             async sendEmailAuthCode() {
                 // 이메일 인증번호 보내기 요청
-                await axios.post('/users/email-auth', { email: this.email })
+                await axios.post('/users/email-auth', {email: this.email})
                     .then(response => {
                         alert('이메일로 인증번호가 전송되었습니다.');
                     })
@@ -20,25 +79,77 @@ document.addEventListener("DOMContentLoaded", function () {
                         console.error('이메일 인증번호 요청 에러: ', error);
                     });
             },
+            // 닉네임 폼 확인 메서드
+            async checkExistingNickname() {
+                const nickname = this.nickname;
+                const response = await axios.get('/users/check/nickname', {
+                    params: {nickname},
+                })
+
+                if(response.data) {
+                    this.nicknameErrorMessage = '이미 등록된 닉네임입니다.';
+                } else {
+                    this.nicknameErrorMessage = '';
+                }
+            },
+            // 패스워드 입력 폼 확인 메서드
+            async checkPassword() {
+                const password = this.password;
+
+                if(password.length < 8) {
+                    this.passwordErrorMessage = '비밀번호는 최소 8글자 입니다.'
+                } else {
+                    this.passwordErrorMessage = '';
+                }
+
+            },
+            // 패스워드 체크 입력 폼 확인 메서드
+            async checkPasswordCheck() {
+                const passwordCheck = this.passwordCheck;
+
+                if(this.password != passwordCheck) {
+                    this.passwordCheckErrorMessage = '비밀번호와 비밀번호 확인이 다릅니다.'
+                } else {
+                    this.passwordCheckErrorMessage = '';
+                }
+
+            },
+            // 모든 입력 필드가 유효한지 확인하는 메서드
+            checkFormValidity() {
+                this.isFormValid =
+                    this.accountId.length > 0 &&
+                    this.email.length > 0 &&
+                    this.emailAuthCode.length > 0 &&
+                    this.nickname.length > 0 &&
+                    this.password.length > 0 &&
+                    this.passwordCheck.length > 0 &&
+                    this.accountIdErrorMessage === '' &&
+                    this.emailErrorMessage === '' &&
+                    this.emailAuthCodeErrorMessage === '' &&
+                    this.nicknameErrorMessage === '' &&
+                    this.passwordErrorMessage === '' &&
+                    this.passwordCheckErrorMessage === '';
+            },
             async signup() {
                 // 회원가입 요청
-                await axios.post('/users/signup', {
-                    accountId: this.accountId,
-                    email: this.email,
-                    emailAuthCode: this.emailAuthCode,
-                    nickname: this.nickname,
-                    password: this.password,
-                    passwordCheck: this.passwordCheck
-                })
-                    .then(() => {
-                        alert('회원가입이 완료되었습니다.\n'
-                            + '구해듀오의 회원이 되신 것을 환영합니다.');
-                        location.href = '/login' // 회원가입 완료 후 로그인 페이지로 이동
+                try {
+                    await axios.post('/users/signup', {
+                        accountId: this.accountId,
+                        email: this.email,
+                        emailAuthCode: this.emailAuthCode,
+                        nickname: this.nickname,
+                        password: this.password,
+                        passwordCheck: this.passwordCheck
                     })
-                    .catch(error => {
-                        console.error('회원가입 에러: ', error);
-                    });
+
+                    alert('회원가입이 완료되었습니다.\n'
+                        + '구해듀오의 회원이 되신 것을 환영합니다.');
+                    location.href = '/login' // 회원가입 완료 후 로그인 페이지로 이동
+                } catch (error) {
+                    alert('회원가입 실패: ' + error.response.data.message);
+                }
             }
+
         }
     });
 });
