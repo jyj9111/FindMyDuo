369ebdd1af95574db5906caf74214571f81d71ed
jyj9111 <mrjang9111@gmail.com>
2023-09-12 14:59:26 +0900

    Feat: 티어 이미지 가져오는 과정에서 발생하는 에러 처리를 위한 예외코드 추가

--- src/main/java/com/idle/fmd/global/error/exception/BusinessExceptionCode.java
+++ src/main/java/com/idle/fmd/global/error/exception/BusinessExceptionCode.java
@@ -54,6 +54,7 @@ public enum BusinessExceptionCode {
     DUPLICATED_LOL_NICKNAME_ERROR(HttpStatus.BAD_REQUEST, "이미 등록된 롤 계정입니다."),
     // 챔피언 이미지를 불러오지 못할 경우 예외처리
     CANNOT_BRING_CHAMPION_IMAGE_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "해당 챔피언 이미지를 가져올수 없습니다."),
+    CANNOT_BRING_TIER_IMAGE_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "해당 티어 이미지를 가져올수 없습니다."),
 
 
     // 자유게시판 관련 예외 -----------------------------------------------------------------------------------------------
